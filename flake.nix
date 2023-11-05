c9cf7514efb1a4b2df655a53aa5a26fcfcafc2f3
thrombe <yash.n.thombare620@gmail.com>
2023-11-05 21:48:27 +0530

    added nix stuff

---
+++ flake.nix
@@ -0,0 +1,56 @@
+{
+  description = "yaaaaaaaaaaaaaaaaaaaaa";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    nixpkgs-unstable,
+    flake-utils,
+  }:
+    flake-utils.lib.eachSystem ["x86_64-linux"] (system: let
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+      unstable = import nixpkgs-unstable {
+        inherit system;
+      };
+
+      manifest = (pkgs.lib.importTOML ./Cargo.toml).package;
+    in {
+      packages.default = unstable.rustPlatform.buildRustPackage {
+        pname = manifest.name;
+        version = manifest.version;
+        cargoLock.lockFile = ./Cargo.lock;
+        src = pkgs.lib.cleanSource ./.;
+
+        # - [nix flake rust and pkgconfig](https://discourse.nixos.org/t/nix-and-rust-how-to-use-pkgconfig/17465/3)
+        buildInputs = with pkgs; [
+          openssl
+          xdotool
+        ];
+        nativeBuildInputs = with pkgs; [
+          pkg-config
+        ];
+      };
+
+      devShells.default = pkgs.mkShell {
+        nativeBuildInputs = with pkgs;
+          [
+            unstable.rust-analyzer
+            unstable.rustfmt
+            unstable.clippy
+          ]
+          ++ self.packages."${system}".default.nativeBuildInputs
+          ++ self.packages."${system}".default.buildInputs;
+        shellHook = ''
+          export RUST_BACKTRACE="1"
+        '';
+      };
+    });
+}
